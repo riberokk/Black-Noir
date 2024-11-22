c8f8c174f33672cf970bb5c9e351916dcee1a5ac
Pedro Ribeiro <90459377+riberokk@users.noreply.github.com>
2024-11-22 13:40:23 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+
+let lastScrollPosition = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+const currentScrollPosition = window.scrollY;
+
+if (currentScrollPosition > lastScrollPosition) {
+    // Esconde o menu
+    header.style.top = '-103px';
+} else {
+    // Mostra o menu
+    header.style.top = '0';
+}
+
+lastScrollPosition = currentScrollPosition;
+});
